d421b1412046e9055d75fb03b7afa53d66781aa1
Cedric POULET <cpoulet@student.42.fr>
2017-04-20 18:52:39 +0200

    add verbosity lvl kcosta diahree verbale

--- Corewar/srcs/corewar/operation_3.c
+++ Corewar/srcs/corewar/operation_3.c
@@ -6,7 +6,7 @@
 /*   By: bduron <bduron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 19:54:01 by bduron            #+#    #+#             */
-/*   Updated: 2017/04/20 17:23:44 by cpoulet          ###   ########.fr       */
+/*   Updated: 2017/04/20 18:49:55 by cpoulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,10 @@ void	op_fork(t_vm *v, t_list *process)
 	shift = reverse_bytes(v, PC + 1, 2);
 	shift %= IDX_MOD;
 	if (v->display_mode == 1)
+	{
 		printf("P%5d | fork %d (%d)\n", NPRO, shift, (unsigned int)(PC + shift) % MEM_SIZE);
+		print_adv(v, process, 3);
+	}
 	add_process(v, process, (unsigned int)(PC + shift) % MEM_SIZE);
 	PC += 3;
 	PC %= MEM_SIZE;
@@ -48,7 +51,10 @@ void	op_lfork(t_vm *v, t_list *process)
 
 	shift = reverse_bytes(v, PC + 1, 2);
 	if (v->display_mode == 1)
+	{
 		printf("P%5d | lfork %d (%d)\n", NPRO, shift, (unsigned int)(PC + shift) % MEM_SIZE);
+		print_adv(v, process, 3);
+	}
 	add_process(v, process, (unsigned int)(PC + shift) % MEM_SIZE);
 	PC += 3;
 	PC %= MEM_SIZE;
