e4b38532e1c57d32190d4030ad2db5045e339cab
Kevin COSTA <kcosta@e2r5p19.42.fr>
2017-04-24 14:45:22 +0200

    Clean

--- Corewar/check_asm.sh
+++ Corewar/check_asm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-	echo "usage: ./check_asm.sh [-del] [-exec] [-diff]"
+	echo "usage: ./check_asm.sh [-del] [-exec] [-diff] [-deasm]"
 fi
 
 if [ "$1" == "-del" ]; then
@@ -28,6 +28,19 @@ if [ "$1" == "-exec" ]; then
 fi
 
 if [ "$1" == "-diff" ]; then
+	for filename in ./champions/mine/*.cor; do
+		diff ./champions/${filename##*/} $filename
+	done
+fi
+
+if [ "$1" == "-deasm" ]; then
+	for filename in ./champions/mine/*.cor; do
+		./asm -d $filename
+	done
+	rm -rf ./champions/mine/*.cor
+	for filename in ./champions/mine/*.s; do
+		./asm $filename
+	done
 	for filename in ./champions/mine/*.cor; do
 		diff ./champions/${filename##*/} $filename
 	done
